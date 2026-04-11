7e443573c999aef5757dba8b96fe57e11240ce37
rajeshkasavkar9850-boop <rajeshkasavkar9850@gmail.com>
2026-04-11 12:06:58 +0530

    work on setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ourFirstApp';
+  title = 'I Love Angular';
+  userName : string = `I am Raj`;
+
+  skills : Array<string> = ['HTML','JS','CSS','TS','Angular'];
 }
